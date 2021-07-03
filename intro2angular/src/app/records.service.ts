6861973b7e4b6a78f0e994c00f893bd0b00b7b13
Aayush Malde <asm7yaq@virginia.edu>
2021-07-03 18:47:03 -0400

    save

--- intro2angular/src/app/records.service.ts
+++ intro2angular/src/app/records.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { myRecord } from './app.component';
+import { retry, catchError } from 'rxjs/operators';
 
 export interface myData{
   obj: myRecord[]
@@ -10,12 +11,10 @@ export interface myData{
 @Injectable()
 export class RecordsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getData(): Observable<myData> {
-    //  return this.http.get<myData>('http://localhost:1234/file.php')
-    return of({
-      "obj": [
+    const dataArr = [
         {
             "name": "test",
             "online": true
@@ -29,10 +28,29 @@ export class RecordsService {
             "online": true
         }
     ]
-    }
-      
-      
+    // return of({obj: dataArr});  
+    return this.httpClient.get<myData>('http://localhost:1234/file.php').pipe(
+      retry(1),
+      catchError(this.handleError)
     )
-  }
+    }
+
+      // Error handling 
+  handleError(error: { error: { message: string; }; status: any; message: any; }) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    //test
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
+      //test2
+      //test
+    
 }
 
