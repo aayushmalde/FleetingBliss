b9a706d01a26ad3bc69c30bcc2e19dbbd1017966
Aayush Malde <asm7yaq@virginia.edu>
2021-07-13 10:12:44 -0400

    correct imports/exports

--- intro2angular/src/app/app.module.ts
+++ intro2angular/src/app/app.module.ts
@@ -1,31 +1,44 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecordsService } from './records.service';
-import { HttpClientModule } from '@angular/common/http';
-import { NavigationComponent } from './navigation/navigation.component';
-import { HeaderComponent } from './header/header.component';
-import { MainpageComponent } from './mainpage/mainpage.component';
-import { HeadercountryComponent } from './headercountry/headercountry.component'
 import { CountryValuesService } from './country-values.service';
-
+import { HeaderComponent } from './header/header.component';
+import { HeadercountryComponent } from './headercountry/headercountry.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RecordsService } from './records.service';
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     HeaderComponent,
     MainpageComponent,
-    HeadercountryComponent
+    HeadercountryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
   ],
   providers: [RecordsService, CountryValuesService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
