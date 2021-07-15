4d917262660c80ef68a605755e1806877c746c76
Aayush Malde <asm7yaq@virginia.edu>
2021-07-15 16:39:52 -0400

    displaying countryValue, which is placeholder for future Countries object

--- intro2angular/src/app/mainpage/mainpage.component.ts
+++ intro2angular/src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CountryValuesService } from '../country-values.service';
 
 @Component({
@@ -8,13 +9,28 @@ import { CountryValuesService } from '../country-values.service';
   styleUrls: ['./mainpage.component.css'],
 })
 export class MainpageComponent implements OnInit {
-  country: string = '';
+  countryName: string = '';
+  countryValue1: string = '';
   countryName$: Observable<string> | undefined; // convention to use $ at end for observables
   displayedImage = 'assets/images/horse.jpg';
+
+  //in future have Object called countries with values, so that way using the map in the html we can say map.get(countryName).currency
+  map: Map<string, string> = new Map([
+    ['Spain', 'spainvalue'],
+    ['France', 'englandValue'],
+  ]);
 
   constructor(private countryNameService: CountryValuesService) {}
 
   ngOnInit() {
-    this.countryName$ = this.countryNameService.getCountryValueObs();
+    this.countryName$ = this.countryNameService.getCountryValueObs().pipe(
+      tap((countryName) => {
+        this.countryName = countryName;
+      })
+    );
+  }
+
+  getValue(countryName: string) {
+    return this.map.get(countryName);
   }
 }
