5020e163aeb82209e2377caccc772544da1cbe35
Aayush Malde <asm7yaq@virginia.edu>
2021-07-25 21:49:04 -0400

    used forEach from lodash for weather array traversal

--- intro2angular/src/app/weather-info-model.ts
+++ intro2angular/src/app/weather-info-model.ts
@@ -2,7 +2,16 @@ export interface WeatherInfo {
   daily: any;
   current: any;
 }
-
 export interface current {
   temp: any;
+  weather: any;
+}
+export interface daily {
+  temp: any;
+  weather: any;
+}
+
+export interface weather {
+  main: any;
+  description: any;
 }
--- intro2angular/src/app/weather/weather.component.ts
+++ intro2angular/src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forEach } from 'lodash';
 import { weatherService } from '../weather.service';
 
 @Component({
@@ -10,16 +10,27 @@ import { weatherService } from '../weather.service';
 export class WeatherComponent implements OnInit {
   text = 'app';
   countryName = 'Portugal';
-  weather$: Observable<string> | undefined; // convention to use $ at end for observables
-  temperature: any;
+  current_temp: any;
+  current_description: any;
+  daily_temp: any;
+  daily_description: any;
   weatherInformation: any[] | undefined;
 
   constructor(private getWeatherService: weatherService) {}
 
   ngOnInit() {
     this.getWeatherService.getData().subscribe((data) => {
-      console.log(data.current.temp);
-      this.temperature = data.current.temp;
+      console.log(data.daily[0].weather);
+      this.current_temp = data.current.temp;
+      this.current_description = data.current.weather[0].description;
+      forEach(data.daily, (value) => {
+        this.daily_temp = value.temp.day;
+        console.log(this.daily_temp);
+        forEach(value.weather, (element) => {
+          this.daily_description = element.description;
+          console.log(this.daily_description);
+        });
+      });
     });
   }
 }
