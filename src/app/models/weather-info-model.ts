65b829af776edd9f226c9079da565b621034116d
Aayush Malde <malde.aayush@gmail.com>
2021-08-02 19:17:50 -0400

    specified types in weather model

--- src/app/models/weather-info-model.ts
+++ src/app/models/weather-info-model.ts
@@ -1,17 +1,18 @@
 export interface WeatherInfo {
-  daily: Daily;
-  current: Current;
+  current: CurrentInfo;
+  daily: DailyInfo[];
 }
-export interface Current {
-  temp: any;
-  weather: any;
+export interface CurrentInfo {
+  temp: number;
+  weather: Weather[];
 }
-export interface Daily {
-  temp: any;
-  weather: any;
+export interface DailyInfo {
+  temp: Day;
+  weather: Weather[];
 }
-
+export interface Day {
+  day: number;
+}
 export interface Weather {
-  main: any;
   description: string;
 }
